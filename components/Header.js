5ade71edf44d7d01b06cafc0e7d3244991a2efc4
Naman Bansal <97817910+naman4216@users.noreply.github.com>
2024-05-26 17:56:31 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,106 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Ionicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome5 } from '@expo/vector-icons';
+
 const Header = () => {
-    return (
-        <View style={{
-            backgroundColor: "#003580",
-            height: 60,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center'
-        }}>
-            <Pressable style={{
-                flexDirection: 'row',
-                alignItems: "center",
-                borderWidth: 1,
-                borderRadius: 25,
-                padding: 8,
-                borderColor: "white"
-            }}>
-                <Ionicons name="bed-outline" size={24} color="white" />
-                <Text style={{ fontWeight: "bold", marginLeft: 8, color: "white", fontSize: 15 }}>Stays</Text>
-            </Pressable>
-            <Pressable style={{
-                flexDirection: 'row',
-                alignItems: "center",
-            }}>
-                <Ionicons name="airplane-outline" size={24} color="white" />
-                <Text style={{ fontWeight: "bold", marginLeft: 8, color: "white", fontSize: 15 }}>Flights</Text>
-            </Pressable>
-            <Pressable style={{
-                flexDirection: 'row',
-                alignItems: "center",
-            }}>
-                <AntDesign name="car" size={24} color="white" />
-                <Text style={{ fontWeight: "bold", marginLeft: 8, color: "white", fontSize: 15 }}>Car Rental</Text>
-            </Pressable>
-            <Pressable style={{
-                flexDirection: 'row',
-                alignItems: "center",
-            }}>
-<FontAwesome5 name="uber" size={24} color="white" />
-                <Text style={{ fontWeight: "bold", marginLeft: 8, color: "white", fontSize: 15 }}>Taxi</Text>
-            </Pressable>
-        </View>
-    )
-}
+  return (
+    <View
+      style={{
+        backgroundColor: "#003580",
+        height: 65,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          borderColor: "white",
+          borderWidth: 1,
+          borderRadius: 25,
+          padding: 8,
+        }}
+      >
+        <Ionicons name="bed-outline" size={24} color="white" />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Stays
+        </Text>
+      </Pressable>
 
-export default Header
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          
+        }}
+      >
+       <Ionicons name="ios-airplane-outline" size={26} color="white" />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Flights
+        </Text>
+      </Pressable>
 
-const styles = StyleSheet.create({})
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+         
+        }}
+      >
+        <Ionicons name="car-outline" size={26} color="white" />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Car Rental
+        </Text>
+      </Pressable>
+
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        
+         
+        }}
+      >
+        <FontAwesome5 name="uber" size={26} color="white" />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Taxi
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});
